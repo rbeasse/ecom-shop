cffc3ee588574168af42694390b6f94adc29a985
vagrant <me@ryanbeasse.com>
2013-03-29 23:19:22 +0000

    Checkout started

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -8,16 +8,17 @@ class OrderController < ApplicationController
     product_exists = false
 
     session[:cart].each do | line_item |
-    	if line_item[:product] == @product
-    		line_item[:quantity] += 1
+    	if line_item.product == @product
+    		line_item.quantity += 1
     		product_exists = true
     	end
     end
-	  session[:cart] << { product: @product, quantity: 1 } unless product_exists
+	  session[:cart] << OpenStruct.new({ product: @product, quantity: 1 }) unless product_exists
     redirect_to :back
   end
 
   def checkout
+  	@items = session[:cart]
   end
 
   def invoice
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ EcomShop::Application.routes.draw do
   match "order/to_cart" => "order#to_cart", as: :to_cart
   match "store/category/:id" => "store#category", as: :category
   match "store/search/" => "store#search", as: :search, via: :post
+  match "order/checkout" => "order#checkout", as: :checkout
 
   ActiveAdmin.routes(self)
 
