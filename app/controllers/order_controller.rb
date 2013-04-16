5595cc4844f1b80d2fd0a6000dd303cbef85cc56
vagrant <vagrant@quantal64.(none)>
2013-04-16 03:38:00 +0000

    checkout and paypal are a GO

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -7,53 +7,90 @@ class OrderController < ApplicationController
 
   def checkout
   	@items = session[:cart].items
+    @cart = session[:cart]
     @subtotal = 0
     @provinces = Province.all.collect{|p| [ p.title, p.id ] }
   end
 
-  def purchase
+  def invoice
 
     cart = session[:cart]
     province = Province.find(params[:provinces])
 
-    customer = Client.new
-    customer.name = params[:first_name] + " " + params[:last_name] 
-    customer.address = params[:address]
-    customer.city = params[:city]
-    customer.province = province
+    @customer = Client.new
+    @customer.name = params[:first_name] + " " + params[:last_name] 
+    @customer.address = params[:address]
+    @customer.city = params[:city]
+    @customer.province = province
 
-    order = Order.new
-    order.status = "BEING PROCESSED"
-    order.gst = province.gst
-    order.pst = province.pst
-    order.hst = province.hst
-    order.client = customer
-
-    if customer.save && order.save
-
-      # Add the line items..
-      cart.items.each do |item|
-        ordered_item = OrderedProduct.new
-        product = Product.find(item.id)
-        ordered_item.product = product
-        ordered_item.order = order
-        ordered_item.quantity = product.quantity
-        ordered_item.price = product.price
-        ordered_item.save
-      end
-
-      cart.empty
-      
-      redirect_to root_path, notice: 'Cool' 
-    else
-      redirect_to checkout_path, notice: 'Errtor' 
+    unless @customer.save
+      redirect_to root_path, notice: "Please enter all the fields!"
+      return
     end
 
-  end
+    @order = Order.new
+    @order.status = "UNPAID"
+    @order.gst = province.gst
+    @order.pst = province.pst
+    @order.hst = province.hst
+    @order.client = @customer
 
-  def invoice
-    @items = session[:cart].items
-    @running_total = 0
+    @ordered_items = []
+    cart.items.each do |item|
+      ordered_item = OrderedProduct.new
+      product = Product.find(item.id)
+      ordered_item.product = product
+      ordered_item.order = @order
+      ordered_item.quantity = item.quantity
+      ordered_item.price = product.price
+      @ordered_items << ordered_item
+      ordered_item.save
+    end
+
+    @pst_value ||= 0
+    @hst_value ||= 0
+    @gst_value ||= 0
+
+    @total = @order.total
+    @hst_value = (@total * (@order.hst / 100)).round(2) unless @order.hst.nil?
+    @pst_value = (@total * (@order.pst / 100)).round(2) unless @order.pst.nil?
+    @gst_value = (@total * (@order.gst / 100)).round(2) unless @order.gst.nil?
+
+    @grand_total = @total + @pst_value + @gst_value + @hst_value 
+
+    ActiveMerchant::Billing::Base.mode = :test
+
+    gateway = ActiveMerchant::Billing::PaypalGateway.new(
+      :login => "ryanbeasse_api1.gmail.com",
+      :password => "1366079373",
+      :signature => "AsxssMI.Gh8UZ9dTNM.i80KDJRUcA9ZftFRdCDWkQvNSioO4TZpoMWu4"
+    )
+
+    credit_card = ActiveMerchant::Billing::CreditCard.new(
+      :brand              => params[:brand],
+      :number             => params[:number],
+      :verification_value => params[:verification_value],
+      :month              => params[:month],
+      :year               => params[:year],
+      :first_name         => params[:first_name],
+      :last_name          => params[:last_name]
+    )
+
+    if credit_card.valid?
+      response = gateway.authorize((@grand_total.to_i * 100), credit_card, :ip => "127.0.0.1")
+      if response.success?
+        gateway.capture((@grand_total.to_i * 100), response.authorization)
+      else
+        redirect_to root_path, notice: "#{response.message}"
+        return
+      end
+    else
+      redirect_to root_path, notice: "Error: credit card is not valid. #{credit_card.errors.full_messages.join('. ')}"
+      return
+    end
+    cart.empty  
+    @order.status = "PAID"
+    @order.save
   end
 end
 
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -22,5 +22,4 @@ class Cart
     end
     true
   end
-
 end
